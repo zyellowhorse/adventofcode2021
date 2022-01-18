998c128d995e32e4f96ee7080619bbbe3c7fcc45
zyellowhorse <zacharyyellowhorse@gmail.com>
2022-01-17 18:06:52 -0700

    python day 2 part 2 finished

---
+++ python/Day_2/Calculate_Horizontal_And_Depth_Positions_Part2.py
@@ -0,0 +1,37 @@
+def main():
+    print(Calculate_Horizontal_And_Depth_Positions())
+
+def Calculate_Horizontal_And_Depth_Positions():
+    horizontalPosition, depthPosition, aimPosition = 0,0,0
+    instructions = ""
+
+    with open('input.txt') as f:
+        instructions = f.readlines()
+    
+    for instruction in instructions:
+        movmentCommand = instruction.split(" ")
+        
+        # only works in python 3.10 and above
+        #match movmentCommand[0]:
+        #    case "foward":
+        #        horizontalPosition += int(movmentCommand[1])
+        #        depthPosition += aimPosition * int(movmentCommand[1])
+
+        #    case "up":
+        #        aimPosition -= int(movmentCommand[1])
+
+        #    case "down":
+        #        aimPosition += int(movmentCommand[1])
+
+        if movmentCommand[0] == "forward":
+            horizontalPosition += int(movmentCommand[1])
+            depthPosition += aimPosition * int(movmentCommand[1])
+        elif movmentCommand[0] == "up":
+            aimPosition -= int(movmentCommand[1])
+        elif movmentCommand[0] == "down":
+            aimPosition += int(movmentCommand[1])
+
+    return horizontalPosition * depthPosition
+
+if __name__ == '__main__':
+    main()
